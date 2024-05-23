4c4475f19335a0a31ef907b8e037085ae6431680
OlegBelousov007 <belou5offoleg@yandex.ru>
2024-05-23 14:25:18 +0300

    second page

--- DI-11/MainPage.xaml.cs
+++ DI-11/MainPage.xaml.cs
@@ -10,31 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DI_11
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для MainPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainPage : Window
     {
-        public MainWindow()
+        public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var MainPage = new MainPage();
-            MainPage.Show();    
-            this.Close();
         }
     }
 }
