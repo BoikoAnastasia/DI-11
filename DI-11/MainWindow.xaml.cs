bf779ca2dace3eb1e1d36006f7d0c8c272de57f1
Sasha1Rp <aleksandrlukin28417@gmail.com>
2024-05-23 14:15:12 +0300

    Страница1

--- DI-11/MainWindow.xaml.cs
+++ DI-11/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace DI_11
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
