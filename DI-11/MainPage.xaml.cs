242c5d47ca454559edeadaedec7d071f070828d3
OlegBelousov007 <belou5offoleg@yandex.ru>
2024-06-19 12:06:45 +0300

    new datagrid

--- DI-11/MainPage.xaml.cs
+++ DI-11/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,23 @@ namespace DI_11
     /// </summary>
     public partial class MainPage : Window
     {
+        public const string ConnectionString = "Server=3218EC11\\SSQLSERVER;Database=ZelebobaCP_22.05.24;Trusted_Connection=Yes;";
         public MainPage()
         {
             InitializeComponent();
+
+            SqlConnection connection = new SqlConnection("Server=3218EC11\\SSQLSERVER;Database=ZelebobaCP_22.05.24;Trusted_Connection=Yes;");
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT Name, Id, Surname, PhoneNumber, OrderNumber, Email FROM Clients", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            DataClients.ItemsSource = dataTable.DefaultView;
+
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -34,6 +50,21 @@ namespace DI_11
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("SELECT Name, Id, Surname, PhoneNumber, OrderNumber, Email FROM Clients", connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                DataClients.ItemsSource = dataTable.DefaultView;
+            }
         }
     }
 }
--- DI-11/MainWindow.xaml.cs
+++ DI-11/MainWindow.xaml.cs
@@ -41,5 +41,6 @@ namespace DI_11
         {
 
         }
+
     }
 }
